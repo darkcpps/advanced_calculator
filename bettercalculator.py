bfc332c853eec6fe9c51245c8a8d818cc85be0e8
darkcpps <48479063+darkcpps@users.noreply.github.com>
2020-06-23 17:11:45 +1000

    Add files via upload

---
+++ bettercalculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter your first number"))
+op =  (input ("Enter Operator"))
+num2 = float(input("Enter Your Second Number Please"))
+if op == "+":
+    print (num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Add an actual number or an operator you stupid bitch")
